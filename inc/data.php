4ca4da73cefe4234f1bfdb8b05f2fdd7b935d302
CristianT96 <34288537+Tiantomo@users.noreply.github.com>
2017-12-05 18:30:00 -0500

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,179 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Hamster Book",
+	"img" => "img/media/ham.jpg",
+    "genre" => "Animal Care",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "ASPCA"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+$catalog[102] = [
+    "title" => "Guinea Pig Care",
+    "img" => "img/media/guinea.jpg",
+    "genre" => "Animal Care",
+    "format" => "Ebook",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "ASPCA"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0132350884'
+];
+$catalog[103] = [
+    "title" => "Silly Guinea Pigs",
+    "img" => "img/media/guinea2.jpg",
+    "genre" => "Animal",
+    "format" => "Hardcover",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+        "Some Guy"
+    ],
+    "publisher" => "Some Publisher",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+    "title" => "Big ol Book of Buggos",
+    "img" => "img/media/buggo.jpg",
+    "genre" => "Nature",
+    "format" => ".ePUB",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Some guy in England or something"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Movie A",
+    "img" => "img/media/MovieA.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Winston Groom",
+        "Eric Roth"
+    ],
+    "stars" => [
+        "Tom Hanks",
+        "Rebecca Williams",
+        "Sally Field",
+        "Michael Conner Humphreys"
+    ]
+];
+$catalog[202] = [
+    "title" => "Movie B",
+    "img" => "img/media/MovieB.jpg",
+    "genre" => "Action",
+    "format" => "Blu-ray",
+    "year" => 1999,
+    "category" => "Movies",
+    "director" => "Mike Judge",
+    "writers" => [
+        "William Goldman"
+    ],
+    "stars" => [
+        "Ron Livingston",
+        "Jennifer Aniston",
+        "David Herman",
+        "Ajay Naidu",
+        "Diedrich Bader",
+        "Stephen Root"
+    ]
+];
+$catalog[203] = [
+    "title" => "Movie C",
+    "img" => "img/media/MovieC.jpg",
+    "genre" => "Thriller",
+    "format" => "Blu-ray",
+    "year" => 2011,
+    "category" => "Movies",
+    "director" => "Peter Jackson",
+    "writers" => [
+        "J.R.R. Tolkien",
+        "Fran Walsh",
+        "Philippa Boyens",
+        "Peter Jackson"
+    ],
+    "stars" => [
+        "Miko",
+        "Jennifer Aniston",
+        "David Herman",
+        "Ajay Naidu",
+        "Diedrich Bader",
+        "Stephen Root"
+    ]
+];
+$catalog[204] = [
+    "title" => "Movie D",
+    "img" => "img/media/MovieD.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 1987,
+    "category" => "Movies",
+    "director" => "Rob Reiner",
+    "writers" => [
+        "William Goldman"
+    ],
+    "stars" => [
+        "Cary Elwes",
+        "Mandy Patinkin",
+        "Robin Wright",
+        "Chris Sarandon",
+        "Christopher Guest",
+        "Wallace Shawn",
+        "André the Giant",
+        "Fred Savage",
+        "Peter Falk",
+        "Billy Crystal"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Music A",
+    "img" => "img/media/Sam.jpg",
+    "genre" => "Clasical",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Some dude named Sam"
+];
+$catalog[302] = [
+    "title" => "Random Jazz",
+    "img" => "img/media/Random.jpg",
+    "genre" => "Jazz",
+    "format" => "MP3",
+    "year" => 2015,
+    "category" => "Music",
+    "artist" => "Some dude who likes Jazz"
+];
+$catalog[303] = [
+    "title" => "Some old songs",
+    "img" => "img/media/7.jpg",
+    "genre" => "Classical",
+    "format" => "Vinyl",
+    "year" => 1990,
+    "category" => "Music",
+    "artist" => "DECCA"
+];
+$catalog[304] = [
+    "title" => "Hoosier",
+    "img" => "img/media/hoo.jpg",
+    "genre" => "Jaz",
+    "format" => "MP3",
+    "year" => 2008,
+    "category" => "Music",
+    "artist" => "Nat King Cole"
+];
